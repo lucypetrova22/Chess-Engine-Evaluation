0d1eec2c46402f6f3fdcd3b695083bb1c81828e0
Lucy Petrova <164482879+lucypetrova22@users.noreply.github.com>
2024-11-03 21:53:23 +0200

    Create BasicMoveAnalysis.java

---
+++ BasicMoveAnalysis.java
@@ -0,0 +1,19 @@
+public class BasicMoveAnalysis {
+    private ChessBoard board;
+
+    public BasicMoveAnalysis(ChessBoard board) {
+        this.board = board;
+    }
+
+    public int evaluateBoard() {
+        int score = 0;
+
+        return score;
+    }
+
+    public void suggestMove() {
+
+        System.out.println("Suggesting a basic move...");
+    }
+}
+
